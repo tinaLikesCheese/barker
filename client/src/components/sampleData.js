b78ec940a719d8e6fa812e67ec63d5124b37c712
Tina Le <tinale09@gmail.com>
2019-08-12 14:18:16 -0700

    initial database and schema set up

--- client/src/components/sampleData.js
+++ client/src/components/sampleData.js
@@ -1,11 +1,65 @@
 
 
 
+
 const imgArr = [
   'https://munch-gallery.s3-us-west-1.amazonaws.com/Billy.jpg',
   'https://munch-gallery.s3-us-west-1.amazonaws.com/Christian.jpg', 
   'https://munch-gallery.s3-us-west-1.amazonaws.com/Janice.jpg',
   'https://munch-gallery.s3-us-west-1.amazonaws.com/Joel.jpg'
+];
+
+// roughhouse:'',
+// chase:'',
+// fetch:'',
+// toys:'',
+
+const sampleData = [
+  {
+    name: 'Billy',
+    age: 5, 
+    size: 'Medium',
+    url: 'https://munch-gallery.s3-us-west-1.amazonaws.com/Billy.jpg',
+    likes: {
+      fetch: true, 
+      toys: true,
+      roughhouse: true,
+    },
+    about: 'Lorem ipsum dolor amet aesthetic ipsum velit commodo, cronut slow-carb fingerstache edison bulb ex kitsch qui sed. Austin esse ethical, thundercats reprehenderit laborum four dollar toast tattooed blue bottle velit gluten-free. Dolor succulents cornhole kombucha nisi cred chillwave church-key culpa. Hexagon minim narwhal sunt cray. Jianbing taiyaki laborum truffaut small batch et YOLO, williamsburg excepteur knausgaard sartorial tbh squid.'
+  }, 
+  {
+    name: 'Christian',
+    age: 3, 
+    size: 'Medium',
+    url: 'https://munch-gallery.s3-us-west-1.amazonaws.com/Christian.jpg',
+    likes: {
+      fetch: true, 
+      toys: true,
+    },
+    about: 'Lorem ipsum dolor amet aesthetic ipsum velit commodo, cronut slow-carb fingerstache edison bulb ex kitsch qui sed. Austin esse ethical, thundercats reprehenderit laborum four dollar toast tattooed blue bottle velit gluten-free. Dolor succulents cornhole kombucha nisi cred chillwave church-key culpa. Hexagon minim narwhal sunt cray. Jianbing taiyaki laborum truffaut small batch et YOLO, williamsburg excepteur knausgaard sartorial tbh squid.'
+  },
+  {
+    name: 'Janice',
+    age: 8, 
+    size: 'Small',
+    url: 'https://munch-gallery.s3-us-west-1.amazonaws.com/Janice.jpg',
+    likes: {
+      chase: true, 
+      toys: true,
+    },
+    about: 'Lorem ipsum dolor amet aesthetic ipsum velit commodo, cronut slow-carb fingerstache edison bulb ex kitsch qui sed. Austin esse ethical, thundercats reprehenderit laborum four dollar toast tattooed blue bottle velit gluten-free. Dolor succulents cornhole kombucha nisi cred chillwave church-key culpa. Hexagon minim narwhal sunt cray. Jianbing taiyaki laborum truffaut small batch et YOLO, williamsburg excepteur knausgaard sartorial tbh squid.'
+  },
+  {
+    name: 'Joel',
+    age: 6, 
+    size: 'Small',
+    url: 'https://munch-gallery.s3-us-west-1.amazonaws.com/Joel.jpg',
+    likes: {
+      chase: true, 
+      roughhouse: true,
+    },
+    about: 'Lorem ipsum dolor amet aesthetic ipsum velit commodo, cronut slow-carb fingerstache edison bulb ex kitsch qui sed. Austin esse ethical, thundercats reprehenderit laborum four dollar toast tattooed blue bottle velit gluten-free. Dolor succulents cornhole kombucha nisi cred chillwave church-key culpa. Hexagon minim narwhal sunt cray. Jianbing taiyaki laborum truffaut small batch et YOLO, williamsburg excepteur knausgaard sartorial tbh squid.'
+  }, 
 ]
 
-export default imgArr;
+export default sampleData;
---
+++ server/db/index.js
@@ -0,0 +1,59 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/barker', {useNewUrlParser: true});
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('db connected')
+});
+
+var dogSchema = new mongoose.Schema({
+    name: { 
+      type: String, unique: true 
+    },
+    age: Number, 
+    size: String,
+    url: String,
+    likes: [{
+      type: String
+    }],
+    about: String,
+})
+
+var Dog = mongoose.model('Dog', dogSchema)
+
+
+const save = (dog, callback) => {
+  let doggo = new Dog ({
+    name: dog.name,
+    age: dog.age,
+    size: dog.size,
+    url: dog.url, 
+    likes: dog.likes,
+    about: dog.about,
+  })
+  doggo.save(function (err, dog) {
+    if (err) {
+      callback(err);
+    } else {
+      callback(null, 'success');
+    }
+  })
+};
+
+
+let Billy =
+{
+  name: 'Billy',
+  age: 5, 
+  size: 'Medium',
+  url: 'https://munch-gallery.s3-us-west-1.amazonaws.com/Billy.jpg',
+  likes: ['fetch', 'toys', 'roughhouse'],
+  about: 'Lorem ipsum dolor amet aesthetic ipsum velit commodo, cronut slow-carb fingerstache edison bulb ex kitsch qui sed. Austin esse ethical, thundercats reprehenderit laborum four dollar toast tattooed blue bottle velit gluten-free. Dolor succulents cornhole kombucha nisi cred chillwave church-key culpa. Hexagon minim narwhal sunt cray. Jianbing taiyaki laborum truffaut small batch et YOLO, williamsburg excepteur knausgaard sartorial tbh squid.'
+}
+
+save(Billy, (err, data) => console.log(data));
+
+
+
+
